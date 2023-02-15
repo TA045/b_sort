55663a31115559150945c1061c336f8f2959f94b
ta045 <callas03@gmail.com>
2023-02-15 19:20:44 +0900

    zero

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
   let mut vec:Vec<i32> = Vec::new();
 
   for _n in 1..=1_000 {
-    let r = rand::thread_rng().gen_range(1..1_000);
+    let r = rand::thread_rng().gen_range(0..10);
     vec.push(r);
   }
 
@@ -13,34 +13,41 @@ fn main() {
 
   //println!("{:?}",vec);  
 
-  // init
-
   let mut _ar:[(i32,i32); 1_000] = [(0,0); 1_000];
 
+  let mut zc = 0;
+ 
   let mut _z = 0;
 
-  for _m in 0..=999 {
 
-  // open
+  for _m in 0..=999 {
 
   let _n = vec.pop();
 
     if let Some(v) = _n {
 
-        _z = _ar[v as usize].1;
+      if v == 0 {
 
-        if v == _ar[v as usize].0 {
+        zc+=1
 
-          _z += 1;
+      }
+      
+      _z = _ar[v as usize].1;
 
-        }
+      if v == _ar[v as usize].0 {
 
-        _ar[v as usize] = (v as i32, _z) ;
+        _z += 1;
+
+      }
+
+      _ar[v as usize] = (v as i32, _z) ;
+
     }
+
   }
 
-  
-  //  println!("{:?}", _ar);  
+
+  //println!("{:?}", _ar);  
 
   // sort
 
@@ -48,19 +55,31 @@ fn main() {
 
   for _o in 0..=999 {
 
-    if _ar[_o as usize].0 > 0 {
 
-        ves.push(_ar[_o as usize].0);
+     while zc > 0 {
 
-        let mut _t = _ar[_o as usize].1;
+       ves.push(0);
+       
+       zc -=1;
 
-        while _t > 0 {
+     } 
 
-            ves.push(_ar[_o as usize].0);
+     if _ar[_o as usize].0 > 0 {
 
-            _t -= 1;
-        }
+       ves.push(_ar[_o as usize].0);
+
+       let mut _t = _ar[_o as usize].1;
+
+       while _t > 0 {
+
+         ves.push(_ar[_o as usize].0);
+
+         _t -= 1;
+
+       }
+
      }
+
   }
 
   //println!("{:?}", ves);  
